a35f2a0b77c2b253bf75053663a89d1d12397699
S-Evangelista <stefanystrenner@gmail.com>
2023-10-31 19:16:40 -0300

    .

--- Listas/App.js
+++ Listas/App.js
@@ -4,17 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}> Anota aí ✒️ </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    titulo: {
+      backgroundColor: '#80FEFF',
+      padding: '12px',
+      borderRadius: '10px',
+      fontSize: '70px',
+      fontFamily:'Poppins',
+      color: 'black',
+      margin: '60px',
+    },
+
   },
-});
+);
